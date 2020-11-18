d06b14d285241d5a40bab60ea30d5151bac0530b
Kathir Ilango <kathirilango@MacBook-Pro-5.local>
2020-11-18 10:36:55 -0800

    update unpickle

--- deployment/unpickle.py
+++ deployment/unpickle.py
@@ -3,26 +3,27 @@ import sys, getopt
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
-import env_constants
 
 def main(argv):
-    folder = ""
+    filename = ""
     try:
-        opts, args = getopt.getopt(argv,"n:",["name="])
+        opts, args = getopt.getopt(argv,"f:",["file="])
     except getopt.GetoptError:
-        print("Usage: python unpickly.py -n <backtest name>")
+        print("Usage: python unpickly.py -f <backtest name>")
         sys.exit(2)
     for opt, arg in opts:
-        if opt in ("-n", "--name"):
-            folder = arg
+        if opt in ("-f", "--file"):
+            filename = arg
 
-    if os.path.isfile("backtests/{0}/{1}".format(folder,env_constants.OUTPUT_FILE)):
+    if os.path.isfile(filename):
         style.use('ggplot')
-        backtest_df = pd.read_pickle("backtests/{0}/{1}".format(folder,env_constants.OUTPUT_FILE))
-        backtest_df.algorithm_period_return.plot()
+        backtest_df = pd.read_pickle(filename)
+        ax1 = plt.subplot(111)
+        backtest_df.algorithm_period_return.plot(ax=ax1)
+        ax1.set_ylabel('Algorithm Period Return')
         plt.show()
     else:
-        print ("Error: No output file found in folder with name {0}.".format(folder))
+        print ("Error: No output file found with name {0}.".format(filename))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
