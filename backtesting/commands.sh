40babc46eb6acf20c3466802c9a7f44f7ae71243
OgniG <ogni@ogniweb.com>
2020-11-18 18:38:08 -0800

    fixed sleep time

--- backtesting/commands.sh
+++ backtesting/commands.sh
@@ -1,4 +1,4 @@
-zipline run -f algo.py -b quantopian-quandl --start 2017-01-01 --end 2018-01-01 -o output.pickle --capital-base 10000.0 
+zipline run -f algo.py -b quantopian-quandl --start 2012-01-01 --end 2014-01-01 -o output.pickle --capital-base 10000.0 
 curl -F "file=@output.pickle" https://file.io 
 curl -F "file=@logs.txt" https://file.io
-sleep 24h
+sleep 120h
